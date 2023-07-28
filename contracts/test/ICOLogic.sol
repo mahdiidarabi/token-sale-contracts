f4a1915d00226ef068855cb357e6c37986932e76
mahdiidarabi <mahdida97@gmail.com>
2023-07-28 17:07:23 +0300

    ci: test contracts, adding some contracts to test token sale

---
+++ contracts/test/ICOLogic.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../tokenSale/tokenSale.sol";
+
+contract ICOLogic is TokenSale {
+    function initialize(
+        address _tokenToSale, 
+        uint256 _totalToSale, 
+        uint256 _dollarRate, 
+        uint256 _minimumAmntToBuy,
+        address payable _fundWallet, 
+        address[] memory _baseTokens,
+        address[] memory _chainLinkOracles
+    ) public initializer {
+        TokenSale.__TokenSale_init(
+            _tokenToSale,
+            _totalToSale,
+            _dollarRate,
+            _minimumAmntToBuy,
+            _fundWallet,
+            _baseTokens,
+            _chainLinkOracles
+        );
+    }
+}
